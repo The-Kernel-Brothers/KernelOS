e56f9177e9b2ea5e63d8a37cf04db642ecffac4e
Minionguyjpro <minionguyjpro@gmail.com>
2023-10-22 18:46:09 +0000

    Better file structure

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #assemble boot.s file
-as --32 ./src/boot.s -o ./src/boot.o
+as --32 ./src/boot.s -o ./obj/boot.o
 
 #compile kernel.c file
-gcc -m32 -c ./src/kernel.c -o ./src/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+gcc -m32 -c ./src/kernel.c -o ./obj/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
 #linking the kernel with kernel.o and boot.o files
-ld -m elf_i386 -T ./src/linker.ld ./src/kernel.o ./src/boot.o -o ./src/KernelOS-i386.bin -nostdlib
+ld -m elf_i386 -T ./linker.ld ./obj/kernel.o ./obj/boot.o -o ./build/KernelOS-i386.bin -nostdlib
 
 #check MyOS.bin file is x86 multiboot file or not
-grub-file --is-x86-multiboot ./src/KernelOS-i386.bin
+grub-file --is-x86-multiboot ./build/KernelOS-i386.bin
 
 #building the iso file
 mkdir -p isodir/boot/grub
-cp ./src/KernelOS-i386.bin isodir/boot/KernelOS-i386.bin
-cp ./src/grub.cfg isodir/boot/grub/grub.cfg
+cp ./build/KernelOS-i386.bin isodir/boot/KernelOS-i386.bin
+cp ./grub.cfg isodir/boot/grub/grub.cfg
 mkdir -p build
 grub-mkrescue -o ./build/KernelOS-i386.iso isodir
